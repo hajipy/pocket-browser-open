a119815ab5361e9573a301241cffd3f8560777d6
Kenichi Saito <hajime@studiohff.net>
2020-01-04 05:07:29 +0900

    implement error handling

--- src/index.ts
+++ src/index.ts
@@ -7,10 +7,17 @@ import { PocketAuth } from "./pocketAuth";
         return;
     }
 
-    const pocketAuth = new PocketAuth(process.env.POCKET_CONSUMER_KEY);
-    const requestToken = await pocketAuth.getRequestToken();
-    await pocketAuth.authorizeRequestToken(requestToken);
-    const accessToken = await pocketAuth.exchangeRequestTokenToAccessToken(requestToken);
-    console.log(`requestToken=${requestToken}`);
-    console.log(`accessToken=${accessToken}`);
+    try {
+        const pocketAuth = new PocketAuth(process.env.POCKET_CONSUMER_KEY);
+        const requestToken = await pocketAuth.getRequestToken();
+        await pocketAuth.authorizeRequestToken(requestToken);
+        const accessToken = await pocketAuth.exchangeRequestTokenToAccessToken(requestToken);
+        console.log(`requestToken=${requestToken}`);
+        console.log(`accessToken=${accessToken}`);
+    }
+    catch (error) {
+        console.error(error);
+        process.exit(1);
+        return;
+    }
 })();
--- src/pocketAuth.ts
+++ src/pocketAuth.ts
@@ -16,18 +16,40 @@ export class PocketAuth {
     }
 
     public async getRequestToken(): Promise<string> {
-        const response = await axios.post(
-            "https://getpocket.com/v3/oauth/request",
-            {
-                consumer_key: this.consumerKey,
-                redirect_uri: PocketAuth.redirectUri
-            },
-            {
-                headers: { "X-Accept": "application/json" }
-            }
-        );
+        try {
+            const response = await axios.post(
+                "https://getpocket.com/v3/oauth/request",
+                {
+                    consumer_key: this.consumerKey,
+                    redirect_uri: PocketAuth.redirectUri
+                },
+                {
+                    headers: { "X-Accept": "application/json" }
+                }
+            );
 
-        return response.data.code;
+            return response.data.code;
+        }
+        catch (error) {
+            if (error.response.status === 400 && error.response.headers["x-error-code"] === "138") {
+                throw new Error("Missing consumer key.");
+            }
+            else if (error.response.status === 400 && error.response.headers["x-error-code"] === "140") {
+                throw new Error("Missing redirect url.");
+            }
+            else if (error.response.status === 403 && error.response.headers["x-error-code"] === "152") {
+                throw new Error("Invalid consumer key.");
+            }
+            else if (
+                error.response.status >= 500 && error.response.status < 600 &&
+                error.response.headers["x-error-code"] === "199"
+            ) {
+                throw new Error("Pocket server issue.");
+            }
+            else {
+                throw error;
+            }
+        }
     }
 
     public async authorizeRequestToken(requestToken: string): Promise<void> {
@@ -56,16 +78,50 @@ export class PocketAuth {
     }
 
     public async exchangeRequestTokenToAccessToken(requestToken: string): Promise<string> {
-        const response = await axios.post("https://getpocket.com/v3/oauth/authorize",
-            {
-                code: requestToken,
-                consumer_key: this.consumerKey
-            },
-            {
-                headers: { "X-Accept": "application/json" }
-            }
-        );
+        try {
+            const response = await axios.post("https://getpocket.com/v3/oauth/authorize",
+                {
+                    code: requestToken,
+                    consumer_key: this.consumerKey
+                },
+                {
+                    headers: { "X-Accept": "application/json" }
+                }
+            );
 
-        return response.data.access_token;
+            return response.data.access_token;
+        }
+        catch (error) {
+            if (error.response.status === 400 && error.response.headers["x-error-code"] === "138") {
+                throw new Error("Missing consumer key.");
+            }
+            else if (error.response.status === 403 && error.response.headers["x-error-code"] === "152") {
+                throw new Error("Invalid consumer key.");
+            }
+            else if (error.response.status === 400 && error.response.headers["x-error-code"] === "181") {
+                throw new Error("Invalid redirect uri.");
+            }
+            else if (error.response.status === 400 && error.response.headers["x-error-code"] === "182") {
+                throw new Error("Missing code.");
+            }
+            else if (error.response.status === 400 && error.response.headers["x-error-code"] === "185") {
+                throw new Error("Code not found.");
+            }
+            else if (error.response.status === 403 && error.response.headers["x-error-code"] === "158") {
+                throw new Error("User rejected code.");
+            }
+            else if (error.response.status === 403 && error.response.headers["x-error-code"] === "159") {
+                throw new Error("");
+            }
+            else if (
+                error.response.status >= 500 && error.response.status < 600 &&
+                error.response.headers["x-error-code"] === "199"
+            ) {
+                throw new Error("Pocket server issue.");
+            }
+            else {
+                throw error;
+            }
+        }
     }
 }
