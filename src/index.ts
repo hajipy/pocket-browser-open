9bed6eeeca4a5ee2cb26a4424a892c98d118d29e
Kenichi Saito <hajime@studiohff.net>
2020-01-07 00:20:49 +0900

    implement —host option

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,15 @@
+import * as commander from "commander";
+
 import { PocketAuth } from "./pocketAuth";
 import { PocketGateway } from "./pocketGateway";
 
 (async () => {
+    const program = new commander.Command();
+    program
+        .option("-d, --host <host>", "Only open items from particular host.");
+
+    program.parse(process.argv);
+
     if (process.env.POCKET_CONSUMER_KEY === undefined) {
         console.error("Environment variable POCKET_CONSUMER_KEY is not set.");
         process.exit(1);
@@ -19,6 +27,7 @@ import { PocketGateway } from "./pocketGateway";
             accessToken,
             state: "unread",
             since: new Date(2020, 0, 1),
+            domain: program.host,
         });
         if (items.length > 0) {
             console.log(JSON.stringify(items[0], null, 4));
